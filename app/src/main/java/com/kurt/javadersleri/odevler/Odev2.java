b4394cbda27cece68b0be7c194e4774e46ba21c3
Muhammed Çağrı Kurt <muhammedcagrikurt@gmail.com>
2023-10-26 21:18:51 +0300

    ....

--- app/src/main/java/com/kurt/javadersleri/odevler/Odev2.java
+++ app/src/main/java/com/kurt/javadersleri/odevler/Odev2.java
@@ -3,7 +3,66 @@ package com.kurt.javadersleri.odevler;
 public class Odev2 {
 
     public Double soru1(double km){
-        return 0.0;
+
+        return km*0.621;
     }
+
+    public void soru2(int kenar1, int kenar2){
+        System.out.println("Diktörge'nin Alanı : "+(kenar1*kenar2));
+    }
+
+    public int soru3(int faktoriyel){
+        if(faktoriyel==0){
+            return 1;
+        }
+        return faktoriyel*soru3(faktoriyel-1);
+    }
+
+    public void soru4(){}
+
+    public double soru5(double kenarSayisi){
+        return (((kenarSayisi-2)*180)/kenarSayisi);
+    }
+
+    public int soru6(int gunSayisi){
+       int calismaUcreti=0,calismaSuresi=0;
+        calismaSuresi=gunSayisi*8;
+       if(calismaSuresi>150){
+            calismaUcreti=(150*40)+((calismaSuresi-150)*80);
+       }
+       else{
+           calismaUcreti=calismaSuresi*40;
+       }
+        return calismaUcreti;
+    }
+
+    public int soru7(int sure){
+        int ucret=0;
+        if(sure>1){
+            ucret+=50;
+            for (int i=1;i<sure;i++){
+                ucret+=10;
+            }
+        }
+        else if(sure==1){
+            ucret+=50;
+        }
+        else {
+            System.out.println("Sıfır Girildi");
+        }
+        return ucret;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
--- app/src/main/java/com/kurt/javadersleri/odevler/Odev2Main.java
+++ app/src/main/java/com/kurt/javadersleri/odevler/Odev2Main.java
@@ -3,6 +3,36 @@ package com.kurt.javadersleri.odevler;
 public class Odev2Main {
     public static void main(String[] args) {
         Odev2 o2 = new Odev2();
-        double x = o2.soru1(0.0);
+        double x = o2.soru1(10);
+        System.out.println("Mil : "+x);
+        System.out.println("-----------------");
+
+        o2.soru2(17,13);
+        System.out.println("---------------------");
+
+        int faktoriyel = o2.soru3(5);
+        System.out.println("Faktoriyel : "+faktoriyel);
+        System.out.println("-----------------");
+
+        o2.soru4();
+        System.out.println("-----------------");
+
+        double icaci= o2.soru5(10);
+        System.out.println("İç Açı Toplamı : "+icaci);
+        System.out.println("-----------------");
+
+        int maas = o2.soru6(30);
+        System.out.println("Maaş : "+maas);
+        System.out.println("-----------------");
+
+        int ucret = o2.soru7(5);
+        System.out.println("Otopark Ücreti : "+ucret);
+        System.out.println("-----------------");
+
+
+
+
+
+
     }
 }
